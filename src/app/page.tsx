a6695b886aa37077dae6dfae6b5ff8651d5c819b
abdur-rhaman-babu <abdulaziz139953@gmail.com>
2024-03-04 12:37:21 +0600

    learnign next js

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+        <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>List of products</h1>
+            <h2>product 1</h2>
+            <h2>product 2</h2>
+            <h2>product 3</h2>
+        </div>
+    );
+};
+
+export default page;
